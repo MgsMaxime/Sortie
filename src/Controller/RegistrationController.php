3033f058501fbcadd5081942ca7e733f98d0b844
SandraLamire <sandra.lamire.sl@gmail.com>
2023-03-17 10:20:28 +0100

    Modifier ville et campus - CSS

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,6 +9,7 @@ use App\Form\RegistationFormCSVType;
 use App\Form\RegistrationFormType;
 use App\Model\FileModel;
 use App\Repository\CampusRepository;
+use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,19 +30,26 @@ class RegistrationController extends AbstractController
         UserPasswordHasherInterface $participantPasswordHasher,
         EntityManagerInterface $entityManager,
         CampusRepository $campusRepository,
+        ParticipantRepository $participantRepository,
     ): Response
     {
         $participant = new participant();
         $form = $this->createForm(RegistrationFormType::class, $participant);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $participant->setPassword($participantPasswordHasher->hashPassword($participant, $form->get('plainPassword')->getData()));
-            $participant->setRoles(['ROLE_USER']);
-            $entityManager->persist($participant);
-            $entityManager->flush();
-            $this->addFlash("success", "Utilisateur inscrit !");
-            return $this->redirectToRoute('participant_list');
-        }
+            $existingParticipant = $participantRepository->findOneBy(['mail' => $participant->getMail()]);
+            if (!$existingParticipant) {
+                $participant->setPassword($participantPasswordHasher->hashPassword($participant, $form->get('plainPassword')->getData()));
+                $participant->setRoles(['ROLE_USER']);
+                $entityManager->persist($participant);
+                $entityManager->flush();
+                $this->addFlash("success", "Utilisateur inscrit !");
+                return $this->redirectToRoute('participant_list');
+            } else {
+                $this->addFlash('error', 'Ce mail existe déjà !');
+            }
+    }
+
         $ajoutfileModel = new FileModel();
         $csvForm = $this->createForm(RegistationFormCSVType::class, $ajoutfileModel);
         $csvForm->handleRequest($request);
@@ -55,21 +63,19 @@ class RegistrationController extends AbstractController
                 if (($handle = fopen($file, "r")) !== FALSE) {
                     while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
                         $x++;
-                        // pour sauter la 1 lignes d'entête
+                        // pour sauter la 1ère ligne d'entête
                         if ($x > 1)
                         {
                             $participant = new participant();
-                            $campus = new Campus();
-                            $campus = $campusRepository->find($data[5]);
-                            $participant->setMail($data[0]);
-                            $participant->setPseudo($data[1]);
-                            $participant->setNom($data[2]);
-                            $participant->setPrenom($data[3]);
-                            $participant->setTelephone($data[4]);
-                            $participant->setPassword('eni2023');
-                            $participant->setCampus($campus);
+
+                            $participant->setPseudo($data[0]);
+                            $participant->setNom($data[1]);
+                            $participant->setPrenom($data[2]);
+                            $participant->setTelephone($data[3]);
+                            $participant->setMail($data[4]);
+                            $participant->setCampus($campusRepository->find($data[5]));
                             $participant->setRoles(['ROLE_USER']);
-                            $participant->setPassword($participantPasswordHasher->hashPassword($participant, $data[5]));
+                            $participant->setPassword($participantPasswordHasher->hashPassword($participant, 'eni2023'));
 
                             $entityManager->persist($participant);
                         }
